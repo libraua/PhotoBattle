1cdb8a0b393e895230c83d395932ebe8a0d83657
Alex K <libraua@gmail.com>
2015-05-20 14:16:29 +0200

    Contact profile image added

--- app/src/main/java/com/libraua/photobattle/LoginActivity.java
+++ app/src/main/java/com/libraua/photobattle/LoginActivity.java
@@ -3,36 +3,15 @@ package com.libraua.photobattle;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.ContentResolver;
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.SignInButton;
-import com.libraua.photobattle.photobattle.PlusBaseActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -44,6 +23,7 @@ public class LoginActivity extends PlusBaseActivity {
     private SignInButton mPlusSignInButton;
     private View mSignOutButtons;
     private View mLoginFormView;
+    private CircleImageView mProfilePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +50,7 @@ public class LoginActivity extends PlusBaseActivity {
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
         mSignOutButtons = findViewById(R.id.plus_sign_out_buttons);
+        mProfilePhoto = findViewById(R.id.profile_photo);
     }
 
     /**
@@ -125,6 +106,7 @@ public class LoginActivity extends PlusBaseActivity {
                 revokeAccess();
             }
         });
+
     }
 
     @Override
